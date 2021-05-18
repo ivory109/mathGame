8621a0b04c9131b4d3f70934c78fe52f910e662f
ivorywang <ivory109@gmail.com>
2021-05-18 16:17:40 +0800

    Initial Commit

---
+++ mathGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mathGame
+//
+//  Created by User on 2021/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
